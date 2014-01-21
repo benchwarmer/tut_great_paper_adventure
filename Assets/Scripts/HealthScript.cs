b4554004dbefa2374c56df69d5eb351cd87eaa1f
benchwarmer <dschevyvette@gmail.com>
2014-01-21 13:22:04 -0600

    Initial commit

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handle hitpoints and damages
+/// </summary>
+public class HealthScript : MonoBehaviour
+{
+  /// <summary>
+  /// Total hitpoints
+  /// </summary>
+  public int hp = 1;
+
+  /// <summary>
+  /// Enemy or player?
+  /// </summary>
+  public bool isEnemy = true;
+
+  void OnTriggerEnter2D(Collider2D otherCollider)
+  {
+    // Is this a shot?
+    ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+    if (shot != null)
+    {
+      // Avoid friendly fire
+      if (shot.isEnemyShot != isEnemy)
+      {
+        hp -= shot.damage;
+
+        // Destroy the shot
+        Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+
+        if (hp <= 0)
+        {
+          // Explosion!
+          SpecialEffectsHelper.Instance.Explosion(transform.position);
+          SoundEffectsHelper.Instance.MakeExplosionSound();
+
+          // Dead!
+          Destroy(gameObject);
+        }
+      }
+    }
+  }
+}
