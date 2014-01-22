90bb9e83a230097568605ca0f6b41d48a8a18cef
benchwarmer <dschevyvette@gmail.com>
2014-01-22 17:04:23 -0600

    Dinkin Around

---
+++ Assets/Scripts/EnemyScript_mine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript_mine : MonoBehaviour {
+
+	private WeaponScript[] weapons;
+	// Use this for initialization
+	void Awake() {
+		weapons = GetComponentsInChildren<WeaponScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(WeaponScript weapon in weapons){
+
+			if (weapon != null) {
+				weapon.Attack(true);
+			}
+		}
+	
+	}
+}
