bc931e9ac98491e3b506320989a742c040258ea7
benchwarmer <dschevyvette@gmail.com>
2014-01-25 13:40:09 -0600

    created particle objects.

--- Assets/Scripts/EnemyScript_mine.cs
+++ Assets/Scripts/EnemyScript_mine.cs
@@ -3,20 +3,79 @@ using System.Collections;
 
 public class EnemyScript_mine : MonoBehaviour {
 
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
-	// Use this for initialization
-	void Awake() {
+	
+	void Awake()
+	{
+		// Retrieve the weapon only once
 		weapons = GetComponentsInChildren<WeaponScript>();
+		
+		// Retrieve scripts to disable when not spawn
+		moveScript = GetComponent<MoveScript>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		foreach(WeaponScript weapon in weapons){
-
-			if (weapon != null) {
-				weapon.Attack(true);
+	// 1 - Disable everything
+	void Start()
+	{
+		hasSpawn = false;
+		
+		// Disable everything
+		// -- collider
+		collider2D.enabled = false;
+		// -- Moving
+		moveScript.enabled = false;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = false;
+		}
+	}
+	
+	void Update()
+	{
+		// 2 - Check if the enemy has spawned.
+		if (hasSpawn == false)
+		{
+			if (renderer.IsVisibleFrom(Camera.main))
+			{
+				Spawn();
 			}
 		}
+		else
+		{
+			// Auto-fire
+			foreach (WeaponScript weapon in weapons)
+			{
+				if (weapon != null && weapon.enabled && weapon.CanAttack)
+				{
+					weapon.Attack(true);
+				}
+			}
+			
+			// 4 - Out of the camera ? Destroy the game object.
+			if (renderer.IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
 	
+	// 3 - Activate itself.
+	private void Spawn()
+	{
+		hasSpawn = true;
+		
+		// Enable everything
+		// -- Collider
+		collider2D.enabled = true;
+		// -- Moving
+		moveScript.enabled = true;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = true;
+		}
 	}
 }
